8c764a0267f0301c3f67945a936f6d3471ebcb8d
keipy27 <kevinandris27@gmail.com>
2023-08-02 11:14:20 +1200

    logics to get users'data is implemented and mapping every data to be displayed on the UI

--- src/components/GithubUsers.js
+++ src/components/GithubUsers.js
@@ -1,25 +1,49 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
+
+const url = "https://api.github.com/users";
 
 const GithubUsers = () => {
-  return (
-    <div className='--bg-primary --py2'>
-        <div className="container">
-            <header>
-                <h1 className='--text-center --text-light'>GitHub Users</h1>
-                <div className="--line"></div>
-            </header>
-            <div className="--grid-25 --py">
-                <div className="--card --bg-light --p --flex-start">
-                    <img src="" alt="image"  className='--profile-img --mx'/>
-                    <span>
-                        <h4>Zino</h4>
-                        <a href="">View Profile</a>
-                    </span>
+
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // console.log(data)
+        setUsers(data)
+    };
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+    
+    return (
+        <div className='--bg-primary --py2'>
+            <div className="container">
+                <header>
+                    <h1 className='--text-center --text-light'>GitHub Users</h1>
+                    <div className="--line"></div>
+                </header>
+                <div className="--grid-25 --py">
+                    {users.map((user) => {
+                        const {id, login, avatar_url, html_url} = user;
+
+                        return (
+                            <div key={id} className="--card --bg-light --p --flex-start">
+                                <img src={avatar_url} alt={login}  className='--profile-img --mx'/>
+                                <span>
+                                    <h4>{login}</h4>
+                                    <a href={html_url}>View Profile</a>
+                                </span>
+                            </div>
+                        )
+                    })}
                 </div>
+
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default GithubUsers
